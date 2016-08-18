69123bf1a5e4b4c06e4ad8a9ced3f3ed079fe3f1
Waldemar Quevedo <wally@apcera.com>
2016-08-17 23:48:40 -0700

    Bump default client dial timeout from tests

--- test/test.go
+++ test/test.go
@@ -169,7 +169,7 @@ func createRouteConn(t tLogger, host string, port int) net.Conn {
 
 func createClientConn(t tLogger, host string, port int) net.Conn {
 	addr := fmt.Sprintf("%s:%d", host, port)
-	c, err := net.DialTimeout("tcp", addr, 1*time.Second)
+	c, err := net.DialTimeout("tcp", addr, 2*time.Second)
 	if err != nil {
 		stackFatalf(t, "Could not connect to server: %v\n", err)
 	}
